68568b25faf7800b8bb62e3937fd9ffedc5ad1f8
Harsh Soni <algebra.soni@gmail.com>
2024-02-04 17:40:48 +0000

    project title

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
             </div>
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                      <h4 className='font-mono text-xl text-greay font-bold hidden lg:block'>CROWD FUNDING</h4>
+                      <h4 className='font-mono text-xl text-greay font-bold hidden lg:block'>RAISE UP</h4>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
