92065525531c5c13799d5d7f1caf682c23594d0e
Harsh Soni <algebra.soni@gmail.com>
2024-02-09 11:40:23 +0000

    display contract address at the end

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Install dependencies in root directory
-npm install
+# # Install dependencies in root directory
+# npm install
 
-# Run Hardhat tasks
-npx hardhat node
-npx hardhat compile
-npx hardhat run scripts/deploy.js --network mumbai
+# # Run Hardhat tasks
+# npx hardhat node
+# npx hardhat compile
 
-# Navigate to clients/ directory and install dependencies
-cd client/
-export NODE_OPTIONS="--openssl-legacy-provider"
-npm install next
-npm install
+# Deploy smart contract to Polygon Mumbai testnet
+echo "Deploying contract to Polygon Mumbai test network..."
+DEPLOY_OUTPUT=$(npx hardhat run scripts/deploy.js --network mumbai)
+echo "Contract address: "
+echo "$DEPLOY_OUTPUT"
 
-# Run development client
-npm run dev
+# # Navigate to clients/ directory and install dependencies
+# cd client/
+# export NODE_OPTIONS="--openssl-legacy-provider"
+# npm install next
+# npm install
+
+# # Run development client
+# npm run dev
