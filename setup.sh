95005ae06580f39fb3c945b9cc1cf20b9b9ac28d
Harsh Soni <algebra.soni@gmail.com>
2024-02-09 11:41:32 +0000

    uncomment test code

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# # Install dependencies in root directory
-# npm install
+# Install dependencies in root directory
+npm install
 
-# # Run Hardhat tasks
-# npx hardhat node
-# npx hardhat compile
+# Run Hardhat tasks
+npx hardhat node
+npx hardhat compile
 
 # Deploy smart contract to Polygon Mumbai testnet
 echo "Deploying contract to Polygon Mumbai test network..."
@@ -13,11 +13,11 @@ DEPLOY_OUTPUT=$(npx hardhat run scripts/deploy.js --network mumbai)
 echo "Contract address: "
 echo "$DEPLOY_OUTPUT"
 
-# # Navigate to clients/ directory and install dependencies
-# cd client/
-# export NODE_OPTIONS="--openssl-legacy-provider"
-# npm install next
-# npm install
+# Navigate to clients/ directory and install dependencies
+cd client/
+export NODE_OPTIONS="--openssl-legacy-provider"
+npm install next
+npm install
 
-# # Run development client
-# npm run dev
+# Run development client
+npm run dev
